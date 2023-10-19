95f68aec70afaee3bdfd70948b7f4038755bb7d5
Mark Young <Markyjay@gmail.com>
2023-10-19 19:41:10 +0000

    add solution generator and array to store clues

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 const characters = ["Peter Venkman", "Ray Stantz", "Egon Spengler", "Winston Zeddemore", "Slimer"];
 const ghostTools = ["Proton Pack", "PKE Meter", "Ghost Trap", "Ecto Goggles", "Neutrona Wand"];
 const halloweenAreas = ["Haunted House", "Pumpkin Patch", "Witch's Lair", "Cemetery", "Creepy Carnival"];
+
+// Generate a random solution
+const solution = {
+    character: characters[Math.floor(Math.random() * characters.length)],
+    weapon: weapons[Math.floor(Math.random() * weapons.length)],
+    room: rooms[Math.floor(Math.random() * rooms.length)]
+};
+
+// Create an array to store clues
+const clues = [];
+
+// Display the solution
+document.getElementById("solution-text").textContent = `${solution.character} in the ${solution.room} with the ${solution.weapon}`;
