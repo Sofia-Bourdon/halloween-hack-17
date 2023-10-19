b209e89fa9d49d022e8c23197056b781152c749f
Mark Young <Markyjay@gmail.com>
2023-10-19 19:39:35 +0000

    add characters tools and areas to script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define characters, weapons, and rooms
+
+const characters = ["Peter Venkman", "Ray Stantz", "Egon Spengler", "Winston Zeddemore", "Slimer"];
+const ghostTools = ["Proton Pack", "PKE Meter", "Ghost Trap", "Ecto Goggles", "Neutrona Wand"];
+const halloweenAreas = ["Haunted House", "Pumpkin Patch", "Witch's Lair", "Cemetery", "Creepy Carnival"];
