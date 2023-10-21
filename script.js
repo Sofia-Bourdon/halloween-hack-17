762f2b36460524d784985ed7b067c19c78a39ae1
Enda <endamagennis@gmail.com>
2023-10-21 13:25:49 +0100

    Add selection functionality to character card

--- script.js
+++ script.js
@@ -3,12 +3,61 @@
 const characters = ["Peter Venkman", "Ray Stantz", "Egon Spengler", "Winston Zeddemore", "Abby Yates", "Patty Tolan", "Jillian Holtzmann", "Erin Gilbert"];
 const ghostTools = ["Proton Pack", "PKE Meter", "Ghost Trap", "Ecto Goggles", "Neutrona Wand", "Spirit Box", "REM Pod", "Ghost Book"];
 const rooms = ["Games Room", "Kitchen", "Living Quarters", "Office", "Reception", "Attic", "Basement", "Sleeping Quarters"];
+let clickcounter = 0;
 
 // Function to randomly select an item from a list
 function getRandomItem(list) {
     const randomIndex = Math.floor(Math.random() * list.length);
     return list[randomIndex];
 }
+
+function highlightSelectedRowOnTable() {
+    // get refernce to the table
+    let table = document.getElementById("table");
+    //get refernce to the rows
+    let rows = table.getElementsByTagName("tr");
+
+    //iterate through the rows
+    for (let i = 0; i < rows.length; i++) {
+        let currentRow = table.rows[i];
+
+        //bind click event to the row
+        currentRow.onclick = function() {
+            clickcounter++;
+            this.classList.add("selected");
+            //highlight the selected row
+            console.log("You selected:", this);
+            if (clickcounter === 1) {
+                this.children[0].style.color = "red";
+            } else if (clickcounter === 2) {
+                this.children[0].style.color = "blue";
+            } else if (clickcounter === 3) {
+                this.children[0].style.color = "green";
+            } else {
+                this.classList.remove("selected");
+                this.children[0].style.color = "black";
+                clickcounter = 0;
+            }
+            console.log(this.classList);
+            console.log(clickcounter);
+        }
+    };
+};
+
+// Function to highlight the mousedover location
+function changeOpacityOnHover() {
+    let rooms = document.getElementsByClassName("room");
+
+    for(room of rooms) {
+        room.addEventListener("mouseover", function() {
+            this.style.opacity = "1";
+        });
+
+        room.addEventListener("mouseout", function() {
+            this.style.opacity = "0.5";
+        });
+    }
+};
 
 // Array to store the three randomized items
 const randomItems = [
